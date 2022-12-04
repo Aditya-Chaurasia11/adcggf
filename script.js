cde048ecc1fe4d8f85ba932583c71f71b52fb7b1
shravanikaware <shravanikaware12@gmail.com>
2022-12-04 18:34:28 +0530

    gdsc intro

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-}
+// menu.onclick = () => {
+// 	menu.classList.toggle('bx-x');
+// 	navbar.classList.toggle('open');
+// }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+  })
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+  }))
